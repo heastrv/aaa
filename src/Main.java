bfd5c1ceb27a37a04c285a5e9cf279aa11dfcb92
heastrv <amdregzgsh46@gmail.com>
2022-12-21 19:12:03 +0200

    added method run1();

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        new Main().run();
+        new Main().run1();
 
     }
 
-    private void run() {System.out.println("Hello world!");
+    private void run1() {
+        int nBox = 12;
+        int apples = 45;
+        System.out.printf("В коробке №%d лежит %d",nBox,apples);
     }
 }
